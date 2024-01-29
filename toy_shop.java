e7f0691f84c1b1a357d611a8e0d8878877a28aef
KDA <kuda74ru@gmail.ru>
2024-01-29 20:09:36 +0500

    add class toys

--- toy_shop.java
+++ toy_shop.java
@@ -1,5 +1,29 @@
 package homework_7;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import  java.util.PriorityQueue;
 
 public class toy_shop {
-    
+    public static void main(String[] args) {
+        // MyQueue<ArrayList> queue;
+        // queue = new MyQueue<>();
+
+
+
+    }
+    public class Toys {
+        private Integer toy_id;
+        private String toy_name;
+        private Integer toy_value;
+
+        public Toys(Integer id, String toy_name, Integer toy_value){
+            this.toy_id = toy_id;
+            this.toy_name = toy_name;
+            this.toy_value = toy_value;
+        }
+        
+        public add()
+    }
 }
+
